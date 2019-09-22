1932a8eed3e9ab5cbc874ba3a2d0e572cdc865a7
MrInfinity <owens1427@gmail.com>
2019-09-22 02:08:41 -0400

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+    
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        SetCountText ();
+        SetLivesText ();
+    }
+
+    void FixedUpdate () 
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        rb2d.AddForce (movement * speed);
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.CompareTag ("PickUp"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+
+        if (other.gameObject.CompareTag ("Enemy"))
+        {
+            other.gameObject.SetActive (false);
+            lives = lives - 1;
+            SetLivesText ();
+        }   
+
+        if(lives == 0)
+        {
+            Destroy(this.gameObject);
+            winText.text = "<b>You Lose!</b>\nMade by Austin Owens";
+        }
+    }
+
+    void SetLivesText () 
+    {
+        if (lives == 3) {
+        livesText.text = "Lives: ♡ ♡ ♡";
+        }
+        else if (lives == 2) {
+        livesText.text = "Lives: ♡ ♡";
+        }
+        else if (lives == 1) {
+        livesText.text = "Lives: ♡";
+        }
+        else if (lives == 0) {
+        livesText.text = "Lives: ☹";
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "<i>Count: </i>" + count.ToString ();
+        if (count == 12)
+        {
+            transform.position = new Vector2(61, 0);
+        }
+        if (count >= 20)
+        {
+            Destroy(this.gameObject);
+            winText.text = "<b>You Win!</b>\nMade by Austin Owens";
+        }
+    }
+
+}
---
+++ Slider.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slider : MonoBehaviour
+{
+
+    private bool goRight = true;
+
+    private float speed = 4.0f;
+ 
+     void FixedUpdate () 
+    {
+         if (goRight)
+            {
+            transform.Translate (Vector2.right * speed * Time.deltaTime);
+            }
+         else
+            {
+            transform.Translate (-Vector2.right * speed * Time.deltaTime);
+            }
+         
+         if(transform.position.x >= 11.8f) 
+            {
+            goRight = false;
+            }
+         
+         if(transform.position.x <= -9.75f) 
+            {
+            goRight = true;
+            }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+}
